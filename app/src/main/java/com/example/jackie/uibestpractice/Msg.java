43403df1e4a2e85039d4eb916faa738a783e5d0f
Jackie <jackiesuzing@gmail.com>
2016-08-15 11:22:17 +0800

    First commit

---
+++ app/src/main/java/com/example/jackie/uibestpractice/Msg.java
@@ -0,0 +1,23 @@
+package com.example.jackie.uibestpractice;
+
+/**
+ * Created by jackie on 2016/7/25.
+ */
+public class Msg {
+    public static final int TYPE_RECEIVED=0;
+    public static final int TYPE_SENT=1;
+    private String content;
+    private int type;
+    public Msg(String content,int type){
+        this.content=content;
+        this.type=type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public int getType() {
+        return type;
+    }
+}
